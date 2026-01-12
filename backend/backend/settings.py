aefb8cdf0ef17628ac21b2d5cd529c5fc2df6073
Mouad <mouadaitouchen48@gmail.com>
2026-01-12 19:03:03 +0100

    Fix: add corsheaders to apps and authorize Netlify URL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'app01'
 ]
 
@@ -54,6 +55,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://diabpredict.netlify.app",
 ]
 
 ROOT_URLCONF = 'backend.urls'
